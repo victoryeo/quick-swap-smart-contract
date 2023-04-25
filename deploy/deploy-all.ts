e3a978d53abbfe501b09f0b85c6b041b26fc16d5
yeosiowvic <victoryeo1974@gmail.com>
2023-04-25 20:10:43 +0800

    add principal lock deployment

--- deploy/deploy-all.ts
+++ deploy/deploy-all.ts
@@ -1,6 +1,5 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
-const crypto = require('crypto');
 
 const func: DeployFunction = async function ({
     deployments,
@@ -10,7 +9,7 @@ const func: DeployFunction = async function ({
     const { deploy } = deployments
     const { deployer, recipient } = await getNamedAccounts()
     console.log("deploy from account", deployer)
-    console.log("htlc recipient", recipient)
+    console.log("quickswap recipient", recipient)
 
     args[0] = "T_NAME"
     args[1] = "T_SYMBOL"
@@ -20,9 +19,20 @@ const func: DeployFunction = async function ({
         log: true,
     })
 
-    args[0] = recipient  // quick swap recipient address
-    args[1] = 2          // time gap
-    await deploy("GriefingLock", {
+    args[0] = recipient    // quick swap recipient address
+    args[1] = 200          // time gap
+    const glock = await deploy("GriefingLock", {
+        from: deployer,
+        args: args,
+        log: true,
+    })
+
+    args[0] = glock.address   // griefing lock recipient address
+    args[1] = recipient       // sender
+    args[2] = deployer        // receiver
+    args[3] = 1               // token amount
+    args[4] = 400             // unlock time
+    await deploy("PrincipalLock", {
         from: deployer,
         args: args,
         log: true,
